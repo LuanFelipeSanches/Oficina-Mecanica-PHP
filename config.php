0d72c45e2db32ff4ac5ebcbbdb1b14c637b56d43
LuanFelipeSanches <95869993+LuanFelipeSanches@users.noreply.github.com>
2022-04-17 23:46:44 -0400

    Variaveis globais Arquivo config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+$nome_oficina = "Oficina Sanches";
+$url = "http://localhost/oficina/";
+$endereco = "Rua Exemplo, 123, Campo Grande - MS";
+$telefone= "(67)91234-1223";
+$email = "luanfelipersanches@gmail.com";
+?>
